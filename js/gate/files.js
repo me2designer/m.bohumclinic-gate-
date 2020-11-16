38bf9f583d0a7994b07a784d6f3b66df68478222
me2designer.com <me2designer@naver.com>
2020-11-16 13:59:09 +0900

    초기세팅값

---
+++ js/gate/files.js
@@ -0,0 +1,32 @@
+cache = '?v='+(new Date).getTime();
+
+var files = [
+    '//fonts.peoplelife.co.kr/fonts/NotoSansKR/NotoSansKR.css',
+    '//fonts.peoplelife.co.kr/fonts/HGGGothicssi/HGGGothicssi.css',
+    '//public.peoplelife.co.kr/lib/swiper/swiper.css',
+    '//public.peoplelife.co.kr/css/reset.css',
+    '//public.peoplelife.co.kr/css/common_mo.css',
+    'css/gate/style.css',
+
+    '//public.peoplelife.co.kr/lib/jquery/jquery.js',
+    '//public.peoplelife.co.kr/lib/swiper/swiper.js',
+    '/js/gate/script.js',
+]
+
+for (var i=0; i<files.length; i++){
+    if (files[i].indexOf('.js') != -1){
+        var el = document.createElement('script');
+        el.async = false;
+        el.type = 'text/javascript';
+        el.src = files[i]+cache;
+
+        (document.getElementsByTagName('HEAD')[0]||document.body).appendChild(el);
+    } else if (files[i].indexOf('.css') != -1) {
+        var el = document.createElement('link');
+        el.rel = 'stylesheet';
+        el.type = 'text/css';
+        el.href = files[i]+cache;
+
+        (document.getElementsByTagName('HEAD')[0]||document.body).appendChild(el);
+    }
+}
